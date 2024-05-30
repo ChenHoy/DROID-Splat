5ad945524e6cbc2bc1302376b6e1b72b64c0ccf8
ChenHoy <ox182@uni-heidelberg.de>
2024-05-30 15:42:52 +0200

    update

--- src/gaussian_mapping.py
+++ src/gaussian_mapping.py
@@ -336,7 +336,7 @@ class GaussianMapper(object):
 
         if random_frames is not None:
             n_rand = int(len(self.cameras) * random_frames)
-            self.info(f"Info. Going over {n_rand} instead of {len(self.cameras)} of frames for optimization ...")
+            self.info(f"Info. Going over {n_rand} random frames instead of {len(self.cameras)} of frames for optimization ...")
             if n_rand > self.max_frames_refinement:
                 n_chunks = n_rand // self.max_frames_refinement
                 self.info(
