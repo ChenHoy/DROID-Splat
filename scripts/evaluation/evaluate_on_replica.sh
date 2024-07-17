646b26266eb2bec0e0a0ca6795a4ffdaa06bf23d
ChenHoy <ox182@uni-heidelberg.de>
2024-07-17 14:00:53 +0200

    change defaults on TUM with correct intrinsics. FIX wrong evaluation scripts from old version

--- scripts/evaluation/evaluate_on_replica.sh
+++ scripts/evaluation/evaluate_on_replica.sh
@@ -9,14 +9,8 @@ echo "Start evaluating on Replica dataset..."
 for sc in ${scenes}
 do
   echo Running on $sc ...
-  python run.py slam.input_folder=/media/data/Replica/${sc} slam.dataset=replica configs/data/Replica/base.yaml mode=$MODE slam.evaluate=True hydra.job.name=${EXPNAME}
-  # TODO change configs dependent on mode if wanted
-  # if [[ $MODE == "mono" ]]
-  # then
-    # python run.py slam.input_folder=/media/data/Replica/${sc} slam.dataset=replica configs/data/Replica/base.yaml mode=$MODE slam.evaluate=True hydra.job.name=${EXPNAME}
-  # else
-    # python run.py slam.input_folder=/media/data/Replica/${sc} slam.dataset=replica configs/data/Replica/base.yaml mode=$MODE slam.evaluate=True hydra.job.name=${EXPNAME}
-  # fi
+  # TODO change the data subconfig to Replica/base.yaml
+  python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} mode=$MODE evaluate=True hydra.job.name=${EXPNAME}
   echo $sc done!
 done
 echo All Done!
--- scripts/evaluation/evaluate_on_tum.sh
+++ scripts/evaluation/evaluate_on_tum.sh
@@ -3,31 +3,14 @@
 MODE=$1
 EXPNAME=$2
 
-scenes='desk desk2 room xyz long_office_household'
+# TODO write the commands manually and no loop since we have to switch configs
+scenes='fr1_desk fr1_desk2 fr1_room fr2_xyz fr3_office'
 
 echo "Start evaluating on TUM dataset..."
 for sc in ${scenes};
 do
   echo Running on $sc ...
-  python run.py slam.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} slam.dataset=tumrgbd configs/data/TUM_RGBD/base.yaml mode=$MODE slam.evaluate=True hydra.job.name=${EXPNAME}
-  # if [[ $MODE == "mono" ]]
-  # then
-    # python run.py slam.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} slam.dataset=tumrgbd configs/data/TUM_RGBD/base.yaml mode=$MODE slam.evaluate=True hydra.job.name=${EXPNAME}
-  # else
-    # python run.py slam.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} slam.dataset=tumrgbd configs/data/TUM_RGBD/base.yaml mode=$MODE slam.evaluate=True hydra.job.name=${EXPNAME}
-  # fi
+  # TODO overwrite the data yaml file based on the scene we are in, since all have different intrinsics
+  python run.py data=TUM_RGBD/fr1 data.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} mode=$MODE evaluate=True hydra.job.name=${EXPNAME}
   echo $sc done!
 done
-
-echo Results for all scenes are:
-
-for sc in ${scenes}
-do
-  echo
-  echo For ${sc}:
-  cat ${OUT_DIR}/${sc}/${EXPNAME}/metrics_traj.txt
-  echo
-  # cat ${OUT_DIR}/${sc}/${EXPNAME}/metrics_mesh.txt
-done
-
-echo All Done!
