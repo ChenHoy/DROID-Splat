8dd941a1cedda6ac2da0bd8cba6c66a10052ad71
ChenHoy <ox182@uni-heidelberg.de>
2024-07-17 21:12:52 +0200

    FIX bug that did not allow to accumulate multiple backend updates due to resets

--- src/backend.py
+++ src/backend.py
@@ -28,7 +28,7 @@ class BackendWrapper(torch.nn.Module):
         # When to start optimizing globally
         self.frontend_window = cfg.tracking.frontend.window
         # Dont consider the state of frontend, but start optimizing after warmup frames
-        self.warmup = min(cfg.tracking.backend.get("warmup", 15), cfg.tracking.warmup)
+        self.warmup = max(cfg.tracking.backend.get("warmup", 10), cfg.tracking.warmup)
         # Do a final refinement over all keyframes if wanted
         self.do_refinement = cfg.tracking.backend.get("do_refinement", False)
 
@@ -113,10 +113,11 @@ class Backend:
 
         Derivation: g12 = g2 * g1.inv(), g23 = g3 * g2.inv()  -> g3 = g23 * g2 = g23 * g12 * g1 = g13 * g1
         """
-        g0, g1 = lietorch.SE3.InitFromVec(pose_prev), lietorch.SE3.InitFromVec(pose_cur)
-        dP = g1 * g0.inv()  # Get relative pose in forward direction
-        dP_prev = lietorch.SE3.InitFromVec(self.video.pose_changes[t0:t1])
-        self.video.pose_changes[t0:t1] = (dP * dP_prev).vec()  # You can now get g_cur = dP * g_prev
+        with self.video.get_lock():
+            g0, g1 = lietorch.SE3.InitFromVec(pose_prev), lietorch.SE3.InitFromVec(pose_cur)
+            dP = g1 * g0.inv()  # Get relative pose in forward direction
+            dP_prev = lietorch.SE3.InitFromVec(self.video.pose_changes[t0:t1])
+            self.video.pose_changes[t0:t1] = (dP * dP_prev).vec()  # You can now get g_cur = dP * g_prev
 
     @torch.no_grad()
     def dense_ba(
@@ -148,13 +149,12 @@ class Backend:
         if add_ii is not None and add_jj is not None:
             graph.add_factors(add_ii, add_jj)
 
-        # Sanity check: always reset the pose changes so we dont accidentally detect a change from some previous optimization ...
-        self.video.pose_changes[:] = torch.tensor([0, 0, 0, 0, 0, 0, 1], dtype=torch.float, device=self.device)
-
-        poses_before = self.video.poses[t_start + 1 : t_end].clone()  # Memoize pose before optimization
+        with self.video.get_lock():
+            poses_before = self.video.poses[t_start + 1 : t_end].clone()  # Memoize pose before optimization
         # fix the start point to avoid drift, be sure to use t_start_loop rather than t_start here.
         graph.update_lowmem(t0=t_start + 1, t1=t_end, steps=steps, iters=iters, max_t=t_end, motion_only=motion_only)
-        poses_after = self.video.poses[t_start + 1 : t_end].clone()  # Memoize pose before optimization
+        with self.video.get_lock():
+            poses_after = self.video.poses[t_start + 1 : t_end].clone()  # Memoize pose before optimization
         # Memoize pose change in self.video so other Processes can adapt their datastructures
         self.accumulate_pose_change(poses_before, poses_after, t0=t_start + 1, t1=t_end)
 
@@ -226,12 +226,14 @@ class Backend:
         if add_ii is not None and add_jj is not None:
             graph.add_factors(add_ii, add_jj)
 
-        poses_before = self.video.poses[t_start + 1 : t_end].clone()  # Memoize pose before optimization
+        with self.video.get_lock():
+            poses_before = self.video.poses[t_start + 1 : t_end].clone()  # Memoize pose before optimization
         # fix the start point to avoid drift, be sure to use t_start_loop rather than t_start here.
         graph.update_lowmem(
             t0=t_start_loop + 1, t1=t_end, steps=steps, iters=iters, max_t=t_end, lm=lm, ep=ep, motion_only=motion_only
         )
-        poses_after = self.video.poses[t_start + 1 : t_end].clone()  # Memoize pose before optimization
+        with self.video.get_lock():
+            poses_after = self.video.poses[t_start + 1 : t_end].clone()  # Memoize pose before optimization
         # Memoize pose change in self.video so other Processes can adapt their datastructures
         self.accumulate_pose_change(poses_before, poses_after, t0=t_start + 1, t1=t_end)
 
--- src/slam.py
+++ src/slam.py
@@ -110,7 +110,7 @@ class SLAM:
             # NOTE self.frontend.window is not an accurate representation over which frames we optimize!
             # Because we delete edges of high age, we usually dont optimize over the whole window, but cut off past frames very fast
             # Example: Window might be [0, 25], but we only optimize [15, 25] -> [0, 15] is untouched and could be handled by backend
-            self.backend_warmup = min(self.frontend.window, self.backend.warmup)
+            self.backend_warmup = self.backend.warmup
         else:
             self.backend = None
             self.backend_warmup = 9999
@@ -514,7 +514,9 @@ class SLAM:
     def maybe_reanchor_gaussians(self, threshold: float = 0.02) -> None:
         """Reanchor the Gaussians to follow a big map update.
         For this purpose we simply track the pose changes after a backend optimization."""
-        unit = self.video.pose_changes.clone()
+        with self.video.get_lock():
+            unit = self.video.pose_changes.clone()
+
         unit[:] = torch.tensor([0, 0, 0, 0, 0, 0, 1], dtype=torch.float, device=self.device)
         delta = pose_distance(self.video.pose_changes, unit)
         to_update = (delta > threshold).nonzero().squeeze()  # Check for frames with large updates
@@ -524,7 +526,8 @@ class SLAM:
             and len(to_update) > 0
         ):
             self.gaussian_mapper.reanchor_gaussians(to_update, self.video.pose_changes[to_update])
-            self.video.pose_changes[to_update] = unit  # Reset the pose update
+            with self.video.get_lock():
+                self.video.pose_changes[to_update] = unit  # Reset the pose update
 
     def gaussian_mapping(
         self,
