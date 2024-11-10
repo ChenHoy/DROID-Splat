2df27c7bcab8f79d29990e5e8fe9ff4a930914a3
ChenHoy <ox182@uni-heidelberg.de>
2024-11-10 11:37:26 +0100

    Change scripts to pass the total number of gaussians as max_cap, so we fit the number of the other experiments

--- scripts/evaluation/evaluate_on_replica.sh
+++ scripts/evaluation/evaluate_on_replica.sh
@@ -3,13 +3,50 @@
 MODE=$1
 EXPNAME=$2
 
-scenes="office0 office1 office2 office3 office4 room0 room1 room2"
+# scenes="office0 office1 office2 office3 office4 room0 room1 room2"
+# NOTE we have to substract some, because the number only limits the growdth during densification, but the final number also can surpassed as new frames come in.
+# FIXME we still get many more Gaussians than the vanilla densification strategy, how to change this?
 
 echo "Start evaluating on Replica dataset..."
-for sc in ${scenes}
-do
-  echo Running on $sc ...
-  python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} mode=$MODE evaluate=True backend_every=8 mapper_every=20 hydra.job.name=${EXPNAME}
-  echo $sc done!
-done
+
+sc='office0'
+targets=200000
+echo Running on $sc ...
+python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} mode=$MODE mapping.mcmc.cap_max=${targets} evaluate=True mapping=replica backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+
+sc='office1'
+targets=200000
+echo Running on $sc ...
+python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} mode=$MODE mapping.mcmc.cap_max=${targets} evaluate=True mapping=replica backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+
+sc='office2'
+targets=200000
+echo Running on $sc ...
+python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} mode=$MODE mapping.mcmc.cap_max=${targets} evaluate=True mapping=replica backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+
+sc='office3'
+targets=120000
+echo Running on $sc ...
+python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} mode=$MODE sleep_delay=0.35 mapping.mcmc.cap_max=${targets} evaluate=True mapping=replica backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+
+sc='office4'
+targets=200000
+echo Running on $sc ...
+python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} sleep_delay=0.35 mode=$MODE mapping.mcmc.cap_max=${targets} evaluate=True mapping=replica backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+
+sc='room0'
+targets=100000
+echo Running on $sc ...
+python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} sleep_delay=0.35 mode=$MODE mapping.mcmc.cap_max=${targets} evaluate=True mapping=replica backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+
+sc='room1'
+targets=220000
+echo Running on $sc ...
+python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} mode=$MODE mapping.mcmc.cap_max=${targets} evaluate=True mapping=replica backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+
+sc='room2'
+targets=200000
+echo Running on $sc ...
+python run.py data=Replica/base data.input_folder=/media/data/Replica/${sc} mode=$MODE mapping.mcmc.cap_max=${targets} evaluate=True mapping=replica backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+
 echo All Done!
--- scripts/evaluation/evaluate_on_tum.sh
+++ scripts/evaluation/evaluate_on_tum.sh
@@ -3,21 +3,18 @@
 MODE=$1
 EXPNAME=$2
 
-# TODO write the commands manually and no loop since we have to switch configs
-scenes='fr1_desk fr1_desk2 fr1_room fr2_xyz fr3_office'
+# scenes='fr1_desk fr1_desk2 fr1_room fr2_xyz fr3_office'
+# targets="60000 70000 250000 25000 90000" # Number of Gaussians to match from the other experiments
 
-echo "Start evaluating on TUM dataset..."
-for sc in ${scenes};
-do
-  echo Running on $sc ...
-  # overwrite the data yaml file based on the scene we are in, since all have different intrinsics
-  if [[ "$sc" =~ "fr1" ]]; then
-    python run.py data=TUM_RGBD/fr1 stride=1 data.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} tracking=tum mapping=tum mode=$MODE evaluate=True backend_every=8 mapper_every=20 hydra.job.name=${EXPNAME}
-  elif [[ "$sc" =~ "fr2" ]]; then
-    python run.py data=TUM_RGBD/fr2 stride=1 data.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} tracking=tum mapping=tum mode=$MODE evaluate=True backend_every=8 mapper_every=20 hydra.job.name=${EXPNAME}
-  elif [[ "$sc" =~ "fr3" ]]; then
-    python run.py data=TUM_RGBD/fr3 stride=1 data.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} tracking=tum mapping=tum mode=$MODE evaluate=True backend_every=8 mapper_every=20 hydra.job.name=${EXPNAME}
-  fi
+sc='fr1_desk'
+python run.py data=TUM_RGBD/fr1 stride=1 data.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} tracking=tum mapping=tum mapping.mcmc.cap_max=60000 mode=$MODE evaluate=True backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+sc='fr1_desk2'
+python run.py data=TUM_RGBD/fr1 stride=1 data.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} tracking=tum mapping=tum mapping.mcmc.cap_max=70000 mode=$MODE evaluate=True backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+sc='fr1_room'
+python run.py data=TUM_RGBD/fr1 stride=1 data.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} tracking=tum mapping=tum mapping.mcmc.cap_max=250000 mode=$MODE evaluate=True backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
 
-  echo $sc done!
-done
+sc='fr2_xyz'
+python run.py data=TUM_RGBD/fr2 stride=1 data.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} tracking=tum mapping=tum mapping.mcmc.cap_max=25000 mode=$MODE evaluate=True backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
+
+sc='fr3_office'
+python run.py data=TUM_RGBD/fr3 stride=1 data.input_folder=/media/data/tum_rgbd/benchmark_rgbd/${sc} tracking=tum mapping=tum mapping.mcmc.cap_max=90000 mode=$MODE evaluate=True backend_every=8 mapper_every=20 hydra.job.name=${sc}_${EXPNAME}
